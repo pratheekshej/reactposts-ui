931c1cb3f2161267d9f6ae57eb049a9080c55a77
Pratheeksh Joseph <pratheekshej@gmail.com>
2024-08-03 12:34:17 -0400

    package updates.

--- src/routes/RootLayout.jsx
+++ src/routes/RootLayout.jsx
@@ -1,12 +1,13 @@
+import { Fragment } from 'react'
 import MainHeader from '../components/main-header/MainHeader'
 import { Outlet } from 'react-router-dom'
 
 const RootLayout = () => {
     return (
-        <>
+        <Fragment>
             <MainHeader />
             <Outlet />
-        </>
+        </Fragment>
     )
 }
 
